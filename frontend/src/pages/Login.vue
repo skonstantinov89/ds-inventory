f876c212a2111c3e57bce6ceed6968743b0101a7
Plamen Petkov <plamen_9915@abv.bg>
2022-05-22 20:18:00 +0300

    Adding Input Fields and Learning Quasar

---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,39 @@
+/* eslint-disable */
+<template>
+
+    <div class='flex center'>
+      <q-card align="center">
+        <q-card-section>
+          <img
+            alt="Direct Logo"
+            src="~assets/ds_logo.png"
+            style="width: 400px; height: 400px"
+          />
+
+          <q-form class="q-gutter-md">
+            <q-input label="Username"> </q-input>
+            <q-input label="Password" type="password"> </q-input>
+            <div>
+              <q-btn class="full-width" color="primary" label="Login" rounded>
+              </q-btn>
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+};
+</script>
+
+<style>
+.center {
+
+text-align: center;
+}
+</style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,8 +1,12 @@
 const routes = [
+  // {
+  //   path: "/",
+  //   component: () => import("layouts/MainLayout.vue"),
+  //   children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+  // },
   {
     path: "/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    component: () => import("pages/Login.vue"),
   },
 
   // Always leave this as last one,
