28b487501285d4add4cab454c504dda27173fedb
Plamen Petkov <plamen_9915@abv.bg>
2022-06-16 18:32:18 +0300

    TEst

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,16 +2,16 @@ const routes = [
   {
     path: "/home",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "/test/dashboard", component: () => import("pages/HomePage.vue") }],
+    // children: [{ path: "/test/dashboard", component: () => import("pages/HomePage.vue") }],
   },
   // {
   //   path: "/home",
   //   component: () => import("pages/HomePage.vue"),
   // },
-  // {
-  //   path: "/",
-  //   component: () => import("pages/Login.vue"),
-  // },
+  {
+    path: "/",
+    component: () => import("pages/Login.vue"),
+  },
 
   // Always leave this as last one,
   // but you can also remove it
